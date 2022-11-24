32b494c43ff93492b62e092f91fdea12880952e8
uduse <wzy950618@gmail.com>
2022-11-23 18:45:34 -0700

    feat: update main.py and Dockerfile

--- docker/main.py
+++ docker/main.py
@@ -6,7 +6,7 @@ import os
 import click
 import docker
 from io import BytesIO
-from docker.errors import BuildError
+from docker.errors import BuildError, ImageNotFound, NotFound
 from docker import APIClient
 from pathlib import Path
 
@@ -20,6 +20,9 @@ workspace_root = Path(__file__).parent.parent.resolve()
 curr_dir = Path(__file__).parent.resolve()
 docker_file_path = curr_dir / "Dockerfile"
 shell_file_path = (curr_dir / "shell.sh").resolve()
+image_name = "laygo2_sky130_docker"
+container_name = "laygo2_sky130_container"
+image_version = "0.0.1"
 
 
 @cli.command()
@@ -30,26 +33,61 @@ def build():
         build_gen = docker_api_cli.build(
             path=str(workspace_root),
             dockerfile=str(docker_file_path),
-            tag="laygo2_sky130",
+            tag=image_name,
         )
 
         for line in build_gen:
             line = json.loads(line)
             if "stream" in line:
                 print(line["stream"].strip())
-    print("Docker image built")
+    try:
+        client = docker.from_env()
+        # check if image exists
+        img = client.images.get(image_name)
+        # tag image with latest and the current version
+        client.images.get(img.id).tag(f"{image_name}:latest")
+        client.images.get(img.id).tag(f"{image_name}:{image_version}")
+    except ImageNotFound:
+        raise Exception("Image build failed")
 
 
 @cli.command()
 def run():
     client = docker.from_env()
+    # check if container already running
+    try:
+        container = client.containers.get(container_name)
+        if container.status == "running":
+            print("Container already running")
+            return
+        elif container.status == "exited":
+            container.start()
+            print("Container restarted")
+            return
+        else:
+            print("Container status: ", container.status)
+            return
+    except NotFound:
+        container = None
+
+    # check if image exists
+    try:
+        img = client.images.get(f"uduse/{image_name}:latest")
+    except ImageNotFound:
+        try:
+            img = client.images.get(f"{image_name}:latest")
+        except ImageNotFound:
+            print("Image not found.")
+            return
+
+    print(f"Running docker container using {img.id}")
     # docker run -d -p 6080:80 -v /dev/shm:/dev/shm laygo2_sky130 --name laygo2_sky130_container
     client.containers.run(
-        "laygo2_sky130",
+        img.id,
         detach=True,
         ports={"80": "6080"},
         volumes={"/dev/shm": {"bind": "/dev/shm", "mode": "rw"}},
-        name="laygo2_sky130_container",
+        name=container_name,
     )
     print("Docker container running")
 
@@ -57,14 +95,25 @@ def run():
 @cli.command()
 def shell():
     client = docker.from_env()
-    container = client.containers.get("laygo2_sky130_container")
+    container = client.containers.get(container_name)
     if container.status != "running":
         print("Container is not running")
         print("Starting container")
         run()
     else:
-        command = "docker exec -it laygo2_sky130_container /bin/bash"
-        os.execv('/usr/bin/docker', command.split(" "))
+        command = f"docker exec -it {container_name} /bin/bash"
+        os.execv("/usr/bin/docker", command.split(" "))
+
+
+@cli.command()
+def stop():
+    client = docker.from_env()
+    container = client.containers.get(container_name)
+    if container.status != "running":
+        print("Container is not running")
+        return
+    container.stop()
+    print("Container stopped")
 
 
 if __name__ == "__main__":
